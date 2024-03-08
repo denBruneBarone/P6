67902303c0108300454fab75d69cc3ba713ac4ec
Vi Thien Le <YP02DI@student.aau.dk>
2024-03-08 15:59:08 +0100

    FEATURE: load and split data

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import data_processing.data_cleansing
+from machine_learning.main import train
 
 
 if not os.path.exists("data/datasets/rodrigues/flights_processed.csv"):
@@ -7,3 +8,7 @@ if not os.path.exists("data/datasets/rodrigues/flights_processed.csv"):
     data_processing.data_cleansing.main()
 else:
     print("flights_processed.csv found. Very nice!")
+
+
+if __name__ == "__main__":
+    train()
