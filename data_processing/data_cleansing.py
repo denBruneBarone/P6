1cb0b4d6a668e1842aeaba82aa7f888463153c52
denBruneBarone <runeberhardt@gmail.com>
2024-03-08 10:07:07 +0100

    REFACTOR: Automatically run data_cleansing from main

--- data_processing/data_cleansing.py
+++ data_processing/data_cleansing.py
@@ -1,17 +1,21 @@
-"""
-This module processes the data avaliable at https://doi.org/10.1184/R1/12683453.v1
-"""
-
 import pandas as pd
+import os
+
+
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), "../"))
 
 
 def main():
+    input_file = os.path.join(PROJECT_ROOT, "data/datasets/rodrigues/flights.csv")
+    output_file = os.path.join(PROJECT_ROOT, "data/datasets/rodrigues/flights_processed.csv")
+
     try:
-        data = pd.read_csv('../data/datasets/rodrigues/flights.csv', low_memory=False)
+        data = pd.read_csv(input_file, low_memory=False)
         data = data[((data.route == 'R1') | (data.route == 'R2') | (data.route == 'R3') | (data.route == 'R4') |
                      (data.route == 'R5') | (data.route == 'R6') | (data.route == 'R7'))]
         data_processed = data
-        data_processed.to_csv("../data/datasets/rodrigues/flights_processed.csv", index=False)
+
+        data_processed.to_csv(output_file, index=False)
     except FileNotFoundError:
         print('''
         --------------------------------------------
@@ -19,7 +23,6 @@ def main():
         Please download the file 'flights.csv' from:
         https://doi.org/10.1184/R1/12683453.v1
         --------------------------------------------''')
-
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-if __name__ == '__main__':
-    print('Hello World')
+import os
+import data_processing.data_cleansing
+
+
+if not os.path.exists("data/datasets/rodrigues/flights_processed.csv"):
+    print("flights_processed.csv not found: Generating...")
+    data_processing.data_cleansing.main()
+else:
+    print("flights_processed.csv found. Very nice!")
