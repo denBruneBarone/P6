98749be9588fad04eb4142b551252b7d3ee91994
LucasP <lucas-l-h-p@hotmail.dk>
2024-03-11 09:47:35 +0100

    FEATURE: Created the methods for the class "TrainingDataset"

--- machine_learning/prepare_for_training.py
+++ machine_learning/prepare_for_training.py
@@ -1,5 +1,18 @@
+import torch
+from torch.utils.data import Dataset
 from data_processing.energy_consumption import trapeziod_integration
-class TrainingDataset:
+
+class TrainingDataset(Dataset):
+    def __init__(self, data):
+        self.data = data
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        sample = self.data.iloc[index].values
+        sample = torch.tensor(sample, dtype=torch.float)
+        return sample
     pass
 
 
