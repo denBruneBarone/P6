85ed514afc4f357ad0560cb36281737e75ff545f
denBruneBarone <runeberhardt@gmail.com>
2024-03-15 09:58:34 +0100

    FEATURE: name of variables

--- machine_learning/training.py
+++ machine_learning/training.py
@@ -7,7 +7,7 @@ from machine_learning.config import HPConfig
 
 def training_and_evaluating(train_data, test_data):
     training_dataset = TrainingDataset(train_data)
-    validation_dataset = TrainingDataset(test_data)
+    test_dataset = TrainingDataset(test_data)
 
     # Instantiate the decision tree model with specified hyperparameters
     model = DecisionTreeRegressor(criterion=HPConfig.criterion, max_depth=HPConfig.max_depth,
@@ -29,30 +29,31 @@ def training_and_evaluating(train_data, test_data):
     model.fit(train_features_np, train_targets_np)
 
     print("Evaluating...")
-    # Evaluate on the validation set
-    val_features = []
-    val_targets = []
-    for index in range(len(validation_dataset)):
-        val_input_array, val_target_array = validation_dataset[index]
-        val_features.append(val_input_array)
-        val_targets.append(val_target_array)
+    # Evaluate on the test set
+    test_features = []
+    test_targets = []
+    for index in range(len(test_dataset)):
+        test_input_array, test_target_array = test_dataset[index]
+        test_features.append(test_input_array)
+        test_targets.append(test_target_array)
 
     # Concatenate the lists along the appropriate axis
-    val_features_np = np.concatenate(val_features, axis=0)
-    val_targets_np = np.concatenate(val_targets, axis=0)
+    test_features_np = np.concatenate(test_features, axis=0)
+    test_targets_np = np.concatenate(test_targets, axis=0)
 
-    # Predict on the validation set
-    val_predictions = model.predict(val_features_np)
+    # Predict on the test set
+    test_predictions = model.predict(test_features_np)
 
     # Calculate RMSE for the two output parameters
-    val_rmse = np.sqrt(mean_squared_error(val_targets_np, val_predictions))
-    print(f"Validation Root Mean Squared Error (RMSE) for Voltage and Current: {val_rmse}")
+    # TODO: find rmse på begge targets individuelt
+    test_rmse = np.sqrt(mean_squared_error(test_targets_np, test_predictions))
+    print(f"Test Root Mean Squared Error (RMSE) for Voltage and Current: {test_rmse}")
 
     # Calculate power consumption predictions by multiplying voltage and current predictions
-    power_consumption_predictions = val_predictions[:, 0] * val_predictions[:, 1]
+    power_consumption_predictions = test_predictions[:, 0] * test_predictions[:, 1]
 
     # Calculate the time difference between each timestamp
-    time_diff = np.diff(val_features_np[:, 0], prepend=0)
+    time_diff = np.diff(test_features_np[:, 0], prepend=0)
 
     # Adjust power consumption predictions by multiplying with the time difference
     adjusted_power_consumption = power_consumption_predictions * time_diff
@@ -60,16 +61,16 @@ def training_and_evaluating(train_data, test_data):
     # Calculate cumulative power consumption for each flight
     cumulative_power = np.cumsum(adjusted_power_consumption)
 
-    # Calculate RMSE on the original cumulative power for the validation set
-    val_targets_cumulative_power = np.cumsum(val_targets_np[:, 0] * val_targets_np[:, 1] * time_diff)
-    original_val_rmse = np.sqrt(mean_squared_error(val_targets_cumulative_power, cumulative_power))
+    # Calculate RMSE on the original cumulative power for the test set
+    test_targets_cumulative_power = np.cumsum(test_targets_np[:, 0] * test_targets_np[:, 1] * time_diff)
+    original_test_rmse = np.sqrt(mean_squared_error(test_targets_cumulative_power, cumulative_power))
     # udtryk for forskellen fra cumulative power på ground truth og cumulative power på vores predictions. Jo tættere på nul, jo strammere hul
-    print(f"Original Validation Root Mean Squared Error (RMSE) for Cumulative Power: {original_val_rmse}")
+    print(f"Original Test Root Mean Squared Error (RMSE) for Cumulative Power: {original_test_rmse}")
 
-    # Calculate RMSE on the adjusted cumulative power for the validation set
-    val_rmse = np.sqrt(mean_squared_error(val_targets_np[:, 0] * val_targets_np[:, 1], power_consumption_predictions))
+    # Calculate RMSE on the adjusted cumulative power for the test set
+    test_rmse = np.sqrt(mean_squared_error(test_targets_np[:, 0] * test_targets_np[:, 1], power_consumption_predictions))
     # Udtryk for hvor præcis vores cumulative power på vores predictions er i forhold til sig selv. Store udsving er dårlige.
-    print(f"Adjusted Validation Root Mean Squared Error (RMSE) for Cumulative Power: {val_rmse}")
+    print(f"Adjusted Test Root Mean Squared Error (RMSE) for Cumulative Power: {test_rmse}")
 
     print("Training finished somehow!")
 
