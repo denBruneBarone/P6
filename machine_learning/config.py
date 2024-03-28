2ad168c0f27ada66340a21b196c5b7a27e237144
denBruneBarone <runeberhardt@gmail.com>
2024-03-28 17:11:28 +0100

    REFACTOR: best config

--- machine_learning/config.py
+++ machine_learning/config.py
@@ -1,20 +1,22 @@
 class HPConfig:
-    criterion = 'friedman_mse'  # samme som paper
-    max_depth = 10  # værdien fra paperet om modeller er 7
-    max_features = None  # samme som paper
-    max_leaf_nodes = 500  # værdien fra paperet om modeller er 10
+    criterion = 'friedman_mse'
+    max_depth = 6 # paper siger 7
+    max_features = None
+    max_leaf_nodes = 10
+
 
 class BestHPConfig:
     criterion = 'friedman_mse'
-    max_depth = 8
+    max_depth = 6
     max_features = None
     max_leaf_nodes = 10
+
 
 class GridSearchConfig:
     param_grid = {
         'criterion': ['friedman_mse', 'squared_error'],
         'max_depth': [2, 3, 4, 5, 6, 7, 8],
         'max_features': [None, 'sqrt', 'log2'],
-        'max_leaf_nodes': [2, 3, 4, 5, 6, 7, 8, 9, 10]
+        'max_leaf_nodes': [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 16, 17, 18, 19, 20]
     }
 
