d1b1ee3ccadfd1ca611030354e4697df2265d810
denBruneBarone <runeberhardt@gmail.com>
2024-04-01 17:23:36 +0200

    Feature: nye logs

--- machine_learning/config.py
+++ machine_learning/config.py
@@ -17,6 +17,6 @@ class GridSearchConfig:
         'criterion': ['friedman_mse', 'squared_error'],
         'max_depth': [2, 3, 4, 5, 6, 7, 8],
         'max_features': [None, 'sqrt', 'log2'],
-        'max_leaf_nodes': [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 16, 17, 18, 19, 20]
+        'max_leaf_nodes': [2, 3, 4, 5, 6, 7, 8, 9, 10]
     }
 
