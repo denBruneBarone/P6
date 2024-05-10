d2500ba5d3c0a8d41573ec477cabb0ab19059740
casparejensen <casparejensen@gmail.com>
2024-05-10 14:10:00 +0200

    REFACTOR: Small changes.

--- pathfinding/main.py
+++ pathfinding/main.py
@@ -13,7 +13,7 @@ def setup_workspace(mission):
     space = Workspace(dimensions, max_bounds, mission)
 
     # Add Blockages
-    blockage_matrix_1 = Blockage(50, 50, 5, 150, 150, 0, 'obstacle')
+    blockage_matrix_1 = Blockage(50, 50, 50, 150, 150, 0, 'obstacle')
     space.add_blockage(blockage_matrix_1)
     blockage_matrix_2 = Blockage(50, 50, 30, 0, 100, 0, 'obstacle')
     space.add_blockage(blockage_matrix_2)
@@ -26,13 +26,13 @@ def setup_workspace(mission):
 
 def find_and_show_optimal_path():
     # Define a mission
-    mission = Mission(Node(0, 0, 0), Node(205, 205, 0), 500)
+    mission = Mission(Node(0, 0, 0), Node(207, 207, 30), 100)
 
     workspace = setup_workspace(mission)
 
     # flight_optimal = find_optimal_path(workspace)
     flight_baseline = find_baseline_path(workspace)
-    # print_stats(flight_optimal, flight_baseline)
+    print_stats(flight_baseline)
 
     workspace.add_flight_path(flight_path=flight_baseline)
     # workspace.add_flight_path(flight_path=flight_optimal)
@@ -41,14 +41,22 @@ def find_and_show_optimal_path():
     workspace.plot_space(dimension='3D', dpi=800)
 
 
-def print_stats(flight_optimal, flight_baseline):
-    energy_diff = flight_optimal.energy / flight_baseline.energy * 100
-    if energy_diff > 100:
-        warnings.warn("Baseline cheaper than optimal path!")
+def print_stats(flight_optimal=False, flight_baseline=False):
+    if flight_optimal:
+        print("optimal path", flight_optimal.path)
+        print("optimal energy", flight_optimal.energy)
+    if flight_baseline:
+        print("baseline path", flight_baseline.path)
+        print("baseline energy", flight_baseline.energy)
 
-    print("optimal path", flight_optimal.path)
-    print("baseline path", flight_baseline.path)
-    print(f"optimal / baseline * 100: {energy_diff}")
+    if flight_optimal and flight_baseline:
+        energy_diff = flight_optimal.energy / flight_baseline.energy * 100
+        if energy_diff > 100:
+            warnings.warn("Baseline cheaper than optimal path!")
+
+        print(f"optimal / baseline * 100: {energy_diff}")
+    elif not flight_optimal and not flight_baseline:
+        warnings.warn("Flight_path: No flight paths provided found")
 
 
 if __name__ == '__main__':
